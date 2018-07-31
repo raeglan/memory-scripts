f6b24c975456d4f42678d4f1b6bef68d4228e5e8
Rafael Miranda <r.miranda@neusta.de>
2018-07-31 17:32:25 +0200

    Loggin now contains similarity data

--- data_mapping/data_mapping.js
+++ data_mapping/data_mapping.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin');
 const fs = require('fs'); // filesystem API to write files.
 const ArrayList = require('arraylist');
 
-const serviceAccount = require('./firebase_admin_sdk_key.json');
+const serviceAccount = require('./memory-intelligence-firebase-key');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -16,13 +16,17 @@ const db = admin.firestore();
 db.settings(settings);
 
 // the maximum number of moves a game should have under that and we use 0 padding
-const gameLength = 40;
-const dir = "./logs/";
+const GAME_LENGTH = 40;
+const DIR = "./logs/";
+const SIMILARITY_MATRIX_FILE = "./similarity_blur.json";
 
 // creates the folder specified if it doesn't exist.
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
+if (!fs.existsSync(DIR)) {
+    fs.mkdirSync(DIR);
 }
+
+// gets the similarity matrix
+const similarityMatrix = JSON.parse(fs.readFileSync(SIMILARITY_MATRIX_FILE, 'utf8'));
 
 // First getting all the sessions, they are also filtered, so as only to receive the completed ones. This needs to be
 // improved in the app so as to already save the number and the completion status as an int and a boolean respectively.
@@ -99,14 +103,14 @@ async function handleSession(session, sessionNumber) {
 
     for (log of sortedLogs) {
         // we only want logs without the assistants so we skip the ones with assistants
-        if(log.replayCheat || log.zoomCheat) {
+        if (log.replayCheat || log.zoomCheat) {
             continue;
         }
 
         // this has the whole game data for this log
         const game = new ArrayList();
 
-        // first the mapping for the game moves and the moves themselfes
+        // first the mapping for the game moves and the moves themselves
         const mappedMoves = new ArrayList(); // these are the saved moves in order after the mapping.
         const imageMappings = new Map(); // this is passed to compare with the similarity matrix
         const wholeMapping = new Map(); // this is the one really used to map.
@@ -114,9 +118,8 @@ async function handleSession(session, sessionNumber) {
             if (wholeMapping.get(move)) {
                 mappedMoves.add(wholeMapping.get(move));
             } else {
-                const splittedMove = move.split('.'); // ImageId.PairNumber
-                const imageId = splittedMove[0];
-                const pairNumber = splittedMove[1];
+                const splitMove = move.split('.'); // ImageId.PairNumber
+                const imageId = splitMove[0];
 
                 if (imageMappings.get(imageId)) {
                     const mapped = imageMappings.get(imageId) + ".2"; // must be the second of the pair to appear.
@@ -133,15 +136,34 @@ async function handleSession(session, sessionNumber) {
             }
         }
 
-        // adding all the image mappings to the game
-        for (map of imageMappings.entries()) {
-            game.add(`${map[1]}=${map[0]}`);
+        // Adding the similarity scores for the images used inside this game.
+        // the images in number so we can get them from the matrix
+        const images = Array.from(imageMappings.keys()).map((value) => {
+            return parseInt(value) - 1; // matrix begins in 0, the images at 1, subtracting 1 yields the correct value.
+        });
+        for (let i = 1; i <= imageMappings.size; i++) {
+            for (let k = 1; k <= imageMappings.size; k++) {
+                // skip if we are comparing one to one as the result will always be one, once and for all.
+                if (i >= k)
+                    continue;
+
+                const imageOne = images[i - 1]; // the cards also begin at 1 but the array at 0.
+                const imageTwo = images[k - 1];
+
+                const similarityScore = similarityMatrix[imageOne][imageTwo];
+
+                game.add(`${i}.${k}=${similarityScore}`);
+            }
         }
+        // for (map of imageMappings.entries()) {
+//
+        //     game.add(`${map[1]}=${map[0]}`);
+        // }
 
         // adding {gameLength} moves to the game (if 0 padding if necessary)
-        for (let i = 0; i < gameLength; i++) {
+        for (let i = 0; i < GAME_LENGTH; i++) {
             const move = mappedMoves.get(i);
-            if(move) {
+            if (move) {
                 game.add(move);
             } else {
                 game.add('0');
@@ -149,9 +171,9 @@ async function handleSession(session, sessionNumber) {
         }
 
         // adding timestamps (and 0 padding if necessary)
-        for(let i = 0; i < gameLength; i++) {
+        for (let i = 0; i < GAME_LENGTH; i++) {
             const time = log.timeLog[i];
-            if(time) {
+            if (time) {
                 game.add(time.toDate().getTime().toString());
             } else {
                 game.add('0');
@@ -183,15 +205,12 @@ async function handleSession(session, sessionNumber) {
 
     const date = session.startTime;
     const fileNumber = ("0" + (sessionNumber + 1)).slice(-2); // 01, 02, ..., 12, 13. And so on.
-    const fileName = `${date.getFullYear()}_${date.getMonth()+1}_${date.getDate()}_${fileNumber}`
-    fs.writeFile(dir + fileName + ".txt", sessionText, function(err) {
-        if(err) {
+    const fileName = `${date.getFullYear()}_${date.getMonth() + 1}_${date.getDate()}_${fileNumber}`
+    fs.writeFile(DIR + fileName + ".txt", sessionText, function (err) {
+        if (err) {
             return console.log(err);
         }
 
         console.log("The file ", fileName, " was saved!");
     });
 }
-
-// MAPPING
-// 1.2=0.9, 1.3=0.4, ...
