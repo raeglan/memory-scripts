480ce6264dd3dc586f852ddd88b8926d852c2851
Rafael Miranda <r.miranda@neusta.de>
2018-08-17 12:13:27 +0200

    Started loading files.

--- data_analysis/analysis.js
+++ data_analysis/analysis.js
@@ -1,5 +1,28 @@
-const fs = require('fs'); // filesystem API to read files.
+const fs = require('fs'); // read directory
+const csv = require('csvtojson'); // reading csv files.
+const ArrayList = require('arraylist'); // saving array lists.
 
 // The directory storing all the logs
 const DIR = "./logs/";
 
+readAndAnalyseFiles().then(() => {
+    console.log("Everything completed.");
+}).catch((error) => {
+    console.log("Something went wrong: ", error);
+});
+
+async function readAndAnalyseFiles() {
+    // saving all the objects
+    const logFiles = fs.readdirSync(DIR);
+    const logObjects = new ArrayList();
+
+
+    // converting them to json.
+    for(logFile of logFiles) {
+        const jsonArray = await csv.fromFile(logFile);
+        logObjects.add(jsonArray);
+    }
+
+    // The logs have always the same muster, first 7 data points for the images(ignored), then 40 moves, then 40
+    // timestamps, then game information.
+}
